4bc0a0d3f210e9c290ffd3ba1e88f3e759562a3f
Alex Fu <alex.fu914@gmail.com>
2017-03-16 13:13:40 -0400

    Adapt RxValidator to handle new Validator API.

--- demo/src/main/java/com/alexfu/formvalidator/rx/demo/MainActivity.java
+++ demo/src/main/java/com/alexfu/formvalidator/rx/demo/MainActivity.java
@@ -5,69 +5,91 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alexfu.formvalidator.ValidationResult;
 import com.alexfu.formvalidator.rules.EmailRule;
 import com.alexfu.formvalidator.rules.MinLengthRule;
+import com.alexfu.formvalidator.rx.RxFieldValidationEvent;
+import com.alexfu.formvalidator.rx.RxValidationEvent;
+import com.alexfu.formvalidator.rx.RxValidationState;
 import com.alexfu.formvalidator.rx.RxValidator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
-  private Button validateButton;
-  private EditText firstNameInput, lastNameInput, emailInput;
-  private RxValidator validator = new RxValidator();
+    private Button validateButton;
+    private EditText firstNameInput, lastNameInput, emailInput;
+    RxValidator validator = new RxValidator();
+    List<ValidationResult> validationErrors = new ArrayList<>();
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    setUpViews();
-    setUpValidator();
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpViews();
+        setUpValidator();
+    }
 
-  private void setUpValidator() {
-    validator.addRule(firstNameInput, new MinLengthRule(1, "First name cannot be empty."));
-    validator.addRule(lastNameInput, new MinLengthRule(1, "Last name cannot be empty."));
-    validator.addRule(emailInput, new EmailRule("Invalid email."));
+    private void setUpValidator() {
+        validator.addRule(firstNameInput, new MinLengthRule(1, "First name cannot be empty."));
+        validator.addRule(lastNameInput, new MinLengthRule(1, "Last name cannot be empty."));
+        validator.addRule(emailInput, new EmailRule("Invalid email."));
 
-    // Form validation
+        // Form validation
 
-    validateButton.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        validator.validate().subscribe(subscriber());
-      }
-    });
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                validator.validate();
+            }
+        });
 
-    // Input validation
+        // Input validation
 
-    validator.observe().subscribe(subscriber());
-  }
+        validator.observable().subscribe(subscriber());
+    }
 
-  private Subscriber<ValidationResult> subscriber() {
-    return new Subscriber<ValidationResult>() {
-      @Override public void onCompleted() {
-        // No op
-      }
+    private Subscriber<RxValidationEvent> subscriber() {
+        return new Subscriber<RxValidationEvent>() {
+            @Override public void onCompleted() {
+                // No op
+            }
 
-      @Override public void onError(Throwable error) {
-        error.printStackTrace();
-      }
+            @Override public void onError(Throwable error) {
+                error.printStackTrace();
+            }
 
-      @Override public void onNext(ValidationResult result) {
-        if (result.isValid()) {
-          result.view.setError(null);
-        } else {
-          result.view.setError(result.errors.get(0));
-        }
-      }
-    };
-  }
+            @Override public void onNext(RxValidationEvent event) {
+                if (event.state == RxValidationState.BEGIN) {
+                    validationErrors.clear();
+                }
 
-  private void setUpViews() {
-    firstNameInput = (EditText) findViewById(R.id.first_name_input);
-    lastNameInput = (EditText) findViewById(R.id.last_name_input);
-    emailInput = (EditText) findViewById(R.id.email_input);
-    validateButton = (Button) findViewById(R.id.button_validate);
-  }
+                if (event.state == RxValidationState.FIELD && event instanceof RxFieldValidationEvent) {
+                    ValidationResult result = ((RxFieldValidationEvent) event).result;
+                    if (result.isValid()) {
+                        result.view.setError(null);
+                    } else {
+                        result.view.setError(result.errors.get(0));
+                        validationErrors.add(result);
+                    }
+                }
+
+                if (event.state == RxValidationState.END) {
+                    if (validationErrors.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Form is valid!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        };
+    }
+
+    private void setUpViews() {
+        firstNameInput = (EditText) findViewById(R.id.first_name_input);
+        lastNameInput = (EditText) findViewById(R.id.last_name_input);
+        emailInput = (EditText) findViewById(R.id.email_input);
+        validateButton = (Button) findViewById(R.id.button_validate);
+    }
 }
--- library/src/main/java/com/alexfu/formvalidator/rx/RxValidator.java
+++ library/src/main/java/com/alexfu/formvalidator/rx/RxValidator.java
@@ -1,108 +1,56 @@
 package com.alexfu.formvalidator.rx;
 
-import android.support.v4.util.ArrayMap;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import android.widget.TextView;
 
-import com.alexfu.formvalidator.AbsValidator;
 import com.alexfu.formvalidator.ValidationResult;
-import com.alexfu.formvalidator.ValidationTask;
 import com.alexfu.formvalidator.Validator;
 import com.alexfu.formvalidator.rules.ValidationRule;
 
 import java.util.List;
-import java.util.Map;
 
 import rx.Emitter;
 import rx.Observable;
 import rx.functions.Action1;
-import rx.functions.Cancellable;
-import rx.functions.Func1;
 
 /**
- * A {@link Validator} that provides Rx bindings. Validation happens on the io
- * thread.
+ * A {@link Validator} that provides Rx bindings.
  */
-public class RxValidator extends AbsValidator<Observable<ValidationResult>> {
-  private final Map<View, Observable<TextView>> textWatchers;
+public class RxValidator {
+    final Validator validator = new Validator();
 
-  public RxValidator() {
-    textWatchers = new ArrayMap<>();
-  }
+    public void addRule(TextView view, List<ValidationRule> rules) {
+        validator.addRule(view, rules);
+    }
 
-  /**
-   * Observe validation results from on-the-fly validations.
-   */
-  public Observable<ValidationResult> observe() {
-    return Observable.merge(textWatchers.values())
-        .flatMap(new Func1<TextView, Observable<ValidationResult>>() {
-          @Override public Observable<ValidationResult> call(TextView view) {
-            return validate(view);
-          }
-        });
-  }
+    public void addRule(TextView view, ValidationRule... rules) {
+        validator.addRule(view, rules);
+    }
 
-  @Override public void addRule(TextView view, List<ValidationRule> rules) {
-    super.addRule(view, rules);
-    watchForTextChanges(view);
-  }
+    public void validate() {
+        validator.validate();
+    }
 
-  @Override public void addRule(final TextView view, ValidationRule... rules) {
-    super.addRule(view, rules);
-    watchForTextChanges(view);
-  }
+    public void validate(TextView view) {
+        validator.validate(view);
+    }
 
-  @Override
-  protected Observable<ValidationResult> performValidate(ValidationTask[] tasks) {
-    return Observable.from(tasks).map(map())
-        .compose(new AsyncTransformer<ValidationResult>());
-  }
+    public Observable<RxValidationEvent> observable() {
+        return Observable.fromEmitter(new Action1<Emitter<RxValidationEvent>>() {
+            @Override public void call(final Emitter<RxValidationEvent> emitter) {
+                validator.setCallback(new Validator.Callback() {
+                    @Override public void onFieldValidated(ValidationResult result) {
+                        emitter.onNext(new RxFieldValidationEvent(result));
+                    }
 
-  @Override
-  protected Observable<ValidationResult> performValidate(ValidationTask task) {
-    return Observable.just(task).map(map())
-        .compose(new AsyncTransformer<ValidationResult>());
-  }
+                    @Override public void onBeginFormValidation() {
+                        emitter.onNext(new RxValidationEvent(RxValidationState.BEGIN));
+                    }
 
-  private void watchForTextChanges(final TextView view) {
-    Observable<TextView> observable = Observable.fromEmitter(new Action1<Emitter<TextView>>() {
-      @Override public void call(final Emitter<TextView> emitter) {
-        final TextWatcher watcher = new TextWatcher() {
-          @Override
-          public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            // Ignore
-          }
-
-          @Override
-          public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            // Ignore
-          }
-
-          @Override public void afterTextChanged(Editable editable) {
-            emitter.onNext(view);
-          }
-        };
-
-        emitter.setCancellation(new Cancellable() {
-          @Override public void cancel() throws Exception {
-            view.removeTextChangedListener(watcher);
-          }
-        });
-
-        view.addTextChangedListener(watcher);
-      }
-    }, Emitter.BackpressureMode.DROP);
-
-    textWatchers.put(view, observable);
-  }
-
-  private Func1<ValidationTask, ValidationResult> map() {
-    return new Func1<ValidationTask, ValidationResult>() {
-      @Override public ValidationResult call(ValidationTask task) {
-        return task.validate();
-      }
-    };
-  }
+                    @Override public void onFormValidated() {
+                        emitter.onNext(new RxValidationEvent(RxValidationState.END));
+                    }
+                });
+            }
+        }, Emitter.BackpressureMode.BUFFER);
+    }
 }
